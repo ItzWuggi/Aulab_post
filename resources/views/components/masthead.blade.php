bc8355700db3cb24b38dba3cd09b5414047008f2
Antonio Natoli <antonionatoli420@gmail.com>
2024-05-31 13:50:42 +0200

    scaffolding di base, db connesso

---
+++ resources/views/components/masthead.blade.php
@@ -0,0 +1,10 @@
+<header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-center text-dark">
+          <h1 class="fw-light">The Aulab Post</h1>
+          <p class="lead">Notizie</p>
+        </div>
+      </div>
+    </div>
+  </header>
