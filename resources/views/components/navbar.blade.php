8e370715a2019fc4c2622947e2ad261f256b5923
Antonio Natoli <antonionatoli420@gmail.com>
2024-05-31 14:17:53 +0200

    fortify installato, login e register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,11 +7,19 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="">Features</a>
           </li>
+          @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('register')}}">Registrati</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('login')}}">Accedi</a>
+          </li>
+          @endguest
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
